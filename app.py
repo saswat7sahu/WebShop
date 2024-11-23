6ceaa091d42361508d8685985fc5a8cc645bb616
saswata sahu <saswata2sahu@gmail.com>
2024-11-23 22:30:33 +0530

    added seesion to seller and added consumer request page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,url_for,redirect,flash
+from flask import Flask,render_template,request,url_for,redirect,flash,session
 from app_init import mysql,app
 
 @app.route("/")
@@ -28,13 +28,15 @@ def additems():
         productName=request.form.get('productName')
         productPrice=request.form.get('productPrice')
         productSize=request.form.get('productSize')
+        if "user_id" in session:
+            user_id=session["user_id"]
         # adding record to database
-        cursor =mysql.connection.cursor()
-        cursor.execute('''INSERT INTO product(product_name,productPrice,productSize) VALUES(%s,%s,%s)''',(productName,productPrice,productSize))
-        mysql.connection.commit()
-        cursor.close()
-        flash(f"Item {productName} has been added!",'success')
-        return redirect(url_for('shop'))
+            cursor =mysql.connection.cursor()
+            cursor.execute('''INSERT INTO product(product_name,productPrice,productSize,ProductId) VALUES(%s,%s,%s,%s)''',(productName,productPrice,productSize,user_id))
+            mysql.connection.commit()
+            cursor.close()
+            flash(f"Item {productName} has been added!",'success')
+            return redirect(url_for('shop'))
     return render_template('additems.html')
 @app.route('/login',methods=['GET','POST'])
 def login():
@@ -44,10 +46,14 @@ def login():
 
         # Check if the user exists in the database
         cursor = mysql.connection.cursor()
-        cursor.execute('SELECT username, password FROM signup WHERE username = %s and password= %s', (email,password))
+        cursor.execute('SELECT id,username, password FROM signup WHERE username = %s and password= %s', (email,password))
         user = cursor.fetchone() 
         # If user exists and passwords 
         if user:
+            #session data which user logged in
+            user_id=user[0]
+            print(user_id)
+            session["user_id"]=user_id
             return redirect(url_for('sellerpage'))
         else:
             flash('Incorrect username or password', 'danger')
@@ -70,5 +76,8 @@ def signup():
 @app.route('/sellerpage')
 def sellerpage():
     return render_template("sellerpage.html")
+@app.route('/consumerReq')
+def consumerReq():
+    return render_template("consumerReq.html")
 if __name__ == "__main__":
     app.run()
