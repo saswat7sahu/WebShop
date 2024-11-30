862b14a107f34879d59afb3a2d3c5f25cb252d2c
saswata sahu <saswata2sahu@gmail.com>
2024-11-30 11:53:20 +0530

    added enviorment and db setup

--- app_init.py
+++ app_init.py
@@ -1,13 +1,12 @@
 from flask import Flask
 from flask_mysqldb import MySQL
-
+import os
 app = Flask(__name__, template_folder="./templates")
-app.secret_key = 'secret'
-# Configure MySQL
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '$@$w@T@123'
-app.config['MYSQL_DB'] = 'flask'
-
+app.secret_key = os.getenv('SECRET_KEY', 'default_secret')
+# Configure MySQL using environment variables
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST', 'localhost')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER', 'root')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD', '')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB', 'flask')
 # Initialize MySQL
 mysql = MySQL(app)
